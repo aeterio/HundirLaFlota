1b4ce6fbb090f410ec8b54ff62977100ddcdf3bd
Xabier Gutierrez <xabi809@gmail.com>
2022-03-14 21:33:14 +0100

    Fallos menores
    En el último commit puse una versión del diagrama de clases anterior a la que ha generado el código, ahora deberian concoradar. En la anterior también se me habia olvidado poner la clase vista

---
+++ HundirLaFlota/src/HundirLaFlota/Vista.java
@@ -0,0 +1,63 @@
+package HundirLaFlota;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+
+public class Vista extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Vista frame = new Vista();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Vista() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pnlCentro = new JPanel();
+		contentPane.add(pnlCentro, BorderLayout.CENTER);
+		pnlCentro.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel gridIzq = new JPanel();
+		pnlCentro.add(gridIzq);
+		gridIzq.setLayout(new GridLayout(10, 10, 0, 0));
+		
+		JPanel gridDer = new JPanel();
+		pnlCentro.add(gridDer);
+		gridDer.setLayout(new GridLayout(10, 10, 0, 0));
+		
+		JPanel pnlBajo = new JPanel();
+		contentPane.add(pnlBajo, BorderLayout.SOUTH);
+		
+		JLabel lblMenu = new JLabel("Menu");
+		pnlBajo.add(lblMenu);
+	}
+
+}
